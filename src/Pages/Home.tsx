07c0522b76bc24fdfa36a9ea339dfa536ef901dc
Mekhadev2025 <mekhalnair@gmail.com>
2023-03-26 22:57:56 +0530

    new

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import General from '../Components/General'
-
+import Carousel from '../Components/CarouselEg'
 export default function Home (){
   return (
     <>
     <div>Home</div>
     <General/>
+    <Carousel/>
   </>)
 }
